16a964bec28ad758e2051b832ed2db4d132fb665
hiroaki <hiroaki_m-12a@stu.kbc.ac.jp>
2014-08-21 03:44:35 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
-  root  'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,8 +39,11 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
-      it "should create a user" do
+      describe "after saving the user" do
+
+        it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+        it { should have_link('Sign out') }
       end
     end
   end
